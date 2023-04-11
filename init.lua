394348e8a927ad9cb0436f198b13634427bd31af
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-04-11 17:46:42 -0500

    inlay hitns!

--- init.lua
+++ init.lua
@@ -1,11 +1,32 @@
 return {
   colorscheme = "material",
   lsp = {
+    -- server_registration = function(server, opts)
+    --   if server == 'rust_analyzer' then
+    --     require('rust-tools').setup({ server = opts })
+    --     return
+    --   end
+    --
+    --   require('lspconfig')[server].setup(opts)
+    -- end,
     formatting = { format_on_save = false },
     -- skip_setup = { "rust_analyzer" },
     setup_handlers = {
       -- add custom handler
-      rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end,
+      rust_analyzer = function(_, opts)
+        local rt = require "rust-tools"
+        rt.setup {
+          server = opts,
+          tools = {
+            inlay_hints = {
+              parameter_hints_prefix = "< ",
+              other_hints_prefix = ": ",
+              highlight = "inlays",
+            },
+          },
+        }
+        rt.inlay_hints.enable()
+      end,
     },
     -- server_registration = function(server, opts)
     --   if server == "rust_analyzer" then
