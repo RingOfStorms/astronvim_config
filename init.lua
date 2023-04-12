b8841bf35cd768dee445993c0590dff8872a0295
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-04-12 10:33:06 -0500

    remove cruft

--- init.lua
+++ init.lua
@@ -1,16 +1,7 @@
 return {
   colorscheme = "material",
   lsp = {
-    -- server_registration = function(server, opts)
-    --   if server == 'rust_analyzer' then
-    --     require('rust-tools').setup({ server = opts })
-    --     return
-    --   end
-    --
-    --   require('lspconfig')[server].setup(opts)
-    -- end,
     formatting = { format_on_save = false },
-    -- skip_setup = { "rust_analyzer" },
     setup_handlers = {
       -- add custom handler
       rust_analyzer = function(_, opts)
@@ -28,17 +19,5 @@ return {
         rt.inlay_hints.enable()
       end,
     },
-    -- server_registration = function(server, opts)
-    --   if server == "rust_analyzer" then
-    --     require("rust-tools").setup { server = opts }
-    --     return
-    --   end
-    --
-    --   require("lspconfig")[server].setup(opts)
-    -- end,
-  },
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
   },
 }
