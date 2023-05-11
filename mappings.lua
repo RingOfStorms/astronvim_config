35bd7b51067f1ef855fec8b14375988ee061830e
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-05-11 01:37:11 -0500

    lsp restart, spelling additions

--- mappings.lua
+++ mappings.lua
@@ -50,6 +50,7 @@ return {
     ["<leader>,jr"] = { function() require("jester").run() end, desc = "Run test under cursor" },
     ["<leader>,jf"] = { function() require("jester").run_file() end, desc = "Run tests for file" },
     ["<leader>,jl"] = { function() require("jester").run_last() end, desc = "Run last ran test" },
+    ["<leader>lz"] = { ":LspRestart<CR>", desc = "Restart LSP Server" },
   },
   v = {
     ["<leader>gf"] = { ":OpenInGHFile <CR>", desc = "Open in github" },
